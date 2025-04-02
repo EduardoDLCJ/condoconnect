015dae94eb32584e6ea6517c4ef29df7e630212c
EduardoDLCJ <136216455+EduardoDLCJ@users.noreply.github.com>
2025-04-01 22:01:23 -0600

    agregar registro para calar recuperación

--- src/Login.jsx
+++ src/Login.jsx
@@ -5,14 +5,34 @@ import {jwtDecode} from 'jwt-decode';
 import loadingAnimation from './assets/Animation - 1738769995911.json';
 
 const Login = ({ onLogin }) => {
+      const [formData, setFormData] = useState({
+          nombre: '',
+          apellido: '',
+          correo: '',
+          telefono: '',
+          departamento: '',
+          torre: '',
+          tipoUsuario: 'Dueno',
+          contrasena: ''
+      });
   const [telefono, setTelefono] = useState('');
   const [password, setPassword] = useState('');
   const [showWarning, setShowWarning] = useState(false);
   const [loginError, setLoginError] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [rememberMe, setRememberMe] = useState(false);
+      const [showForm, setShowForm] = useState(false); // Controla si el modal está visible
 
   const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+        ...formData,
+        [name]: value
+    });
+};
+
 
   useEffect(() => {
     document.title = "Inicio de sesión";
@@ -98,6 +118,45 @@ const Login = ({ onLogin }) => {
     }
   };
 
+  const handleRegistro = async (e) => {
+    e.preventDefault();
+    try {
+        const response = await fetch('https://apicondominio-7jd1.onrender.com/registro', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        });
+        const data = await response.json();
+        if (!data) {
+            alert('Error al registrar el usuario: ' + data.message);
+            return;
+        }
+        setFormData({
+            nombre: '',
+            apellido: '',
+            correo: '',
+            telefono: '',
+            departamento: '',
+            torre: '',
+            tipoUsuario: 'Dueno',
+            contrasena: ''
+        });
+        if (response.ok) {
+            alert('Usuario registrado exitosamente');
+            setShowForm(false); 
+        }
+        else {
+            alert('Error al registrar el usuario: ' + data.message);
+        }
+
+        window.location.reload();
+    } catch (error) {
+        console.error('Error:', error);
+    }
+};
+
   return (
     <div className="flex items-center justify-center min-h-screen fondo">
       <div className="flex flex-col items-center">
@@ -168,7 +227,17 @@ const Login = ({ onLogin }) => {
               )}
             </div>
           </form>
+                    
+          <div className="flex justify-center pt-5 space-x-2">
 
+            <button
+            onClick={() => setShowForm(true)}
+
+              className="text-blue-500 hover:underline focus:outline-none"
+            >
+              Registro
+            </button>
+          </div>
           <div className="flex justify-center pt-5 space-x-2">
             <h1 className="text-sm text-gray-700">¿Olvidaste la tu contraseña?</h1>
             <button
@@ -177,6 +246,131 @@ const Login = ({ onLogin }) => {
             >
               Recuperar
             </button>
+
+            {showForm && (
+                        <div className="fixed inset-0 flex items-center justify-center z-50" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+                            <div className="bg-white shadow-xl rounded-lg p-6 w-full max-w-md">
+                                <h2 className="text-xl font-semibold mb-4 text-center text-blue-600">Registrar Usuario</h2>
+                                <form className="space-y-4" onSubmit={handleSubmit}>
+                                    <div>
+                                        <label className="block text-gray-700 text-sm font-semibold mb-2" htmlFor="nombre">
+                                            Nombre
+                                        </label>
+                                        <input
+                                            className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                            id="nombre"
+                                            type="text"
+                                            name="nombre"
+                                            value={formData.nombre}
+                                            onChange={handleChange}
+                                            required
+                                        />
+                                    </div>
+                                    <div>
+                                        <label className="block text-gray-700 text-sm font-semibold mb-2" htmlFor="apellido">
+                                            Apellido
+                                        </label>
+                                        <input
+                                            className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                            id="apellido"
+                                            type="text"
+                                            name="apellido"
+                                            value={formData.apellido}
+                                            onChange={handleChange}
+                                            required
+                                        />
+                                    </div>
+                                    <div>
+                                        <label className="block text-gray-700 text-sm font-semibold mb-2" htmlFor="correo">
+                                            Correo Electrónico
+                                        </label>
+                                        <input
+                                            className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                            id="correo"
+                                            type="email"
+                                            name="correo"
+                                            value={formData.correo}
+                                            onChange={handleChange}
+                                            required
+                                        />
+                                    </div>
+                                    <div>
+                                        <label className="block text-gray-700 text-sm font-semibold mb-2" htmlFor="telefono">
+                                            Teléfono
+                                        </label>
+                                        <input
+                                            className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                            id="telefono"
+                                            type="text"
+                                            name="telefono"
+                                            value={formData.telefono}
+                                            onChange={handleChange}
+                                            required
+                                        />
+                                    </div>
+                                    <div className="flex space-x-4">
+                                  <div className="flex-grow">
+                                      <label className="block text-gray-700 text-sm font-semibold mb-2" htmlFor="departamento">
+                                          Departamento
+                                      </label>
+                                      <input
+                                          className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                          id="departamento"
+                                          type="number"
+                                          name="departamento"
+                                          value={formData.departamento}
+                                          onChange={handleChange}
+                                      />
+                                  </div>
+                                  <div className="flex-grow">
+                                      <label className="block text-gray-700 text-sm font-semibold mb-2" htmlFor="torre">
+                                          Torre
+                                      </label>
+                                      <input
+                                          className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                          id="torre"
+                                          type="number"
+                                          name="torre"
+                                          value={formData.torre}
+                                          onChange={handleChange}
+                                      />
+                                  </div>
+                              </div>
+
+
+                                    <div>
+                                        <label className="block text-gray-700 text-sm font-semibold mb-2" htmlFor="contrasena">
+                                            Contraseña
+                                        </label>
+                                        <input
+                                            className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                            id="contrasena"
+                                            type="password"
+                                            name="contrasena"
+                                            value={formData.contrasena}
+                                            onChange={handleChange}
+                                            required
+                                        />
+                                    </div>
+                                    <div className="flex justify-end space-x-4">
+                                        <button
+                                            className="bg-gray-400 text-white px-4 py-2 rounded-lg hover:bg-gray-500"
+                                            onClick={() => setShowForm(false)}
+                                            type="button"
+                                        >
+                                            Cancelar
+                                        </button>
+                                        <button
+                                            className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700"
+                                            onClick={handleRegistro}
+                                        >
+                                            Registrar
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    )}
           </div>
         </div>
       </div>
